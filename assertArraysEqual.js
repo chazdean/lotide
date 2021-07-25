761ac7b6fb66d8a861dab1d751a8eaba0dadbfe9
chazdean <chaz.dean@shopify.com>
2021-07-25 22:13:01 +0000

    created array assertion

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  let result = true;
+  for (let x = 0; x < arrayOne.length; x++) {
+    if (arrayOne[x] !== arrayTwo[x]) {
+      result = false;
+    }
+  }
+  if (result === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should PASS
