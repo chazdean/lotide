5460ba108c48005c010a20a7cc8b9bb583ed704d
chazdean <chaz.dean@shopify.com>
2021-07-26 22:41:42 +0000

    modified function to utlize eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,15 @@
-const assertArraysEqual = function(arrayOne, arrayTwo) {
+const eqArrays = function(arrayOne, arrayTwo) {
   let result = true;
   for (let x = 0; x < arrayOne.length; x++) {
     if (arrayOne[x] !== arrayTwo[x]) {
       result = false;
     }
   }
-  if (result === true) {
+  return result;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
     console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
@@ -13,6 +17,6 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
 };
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
