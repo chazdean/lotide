abe8ef4e3ed9186a6544e1d740a24c62c0dbc56e
chazdean <chaz.dean@shopify.com>
2021-08-11 02:30:25 +0000

    refactor

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const { exist } = require("joi");
-
-// TEST/ASSERTION FUNCTIONS
-const eqArrays = function(arrayOne, arrayTwo) {
-  let result = true;
-  for (let x = 0; x < arrayOne.length; x++) {
-    if (arrayOne[x] !== arrayTwo[x]) {
-      result = false;
-    }
-  }
-  return result;
-};
-
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
-  }
-};
-
-// ACTUAL FUNCTION
 const middle = function(array) {
   let newArray = [];
   let middleOfArray = array.length / 2;
@@ -34,11 +12,4 @@ const middle = function(array) {
   return newArray;
 };
 
-// TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
+module.exports = middle;
