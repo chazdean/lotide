bdf7bdcb5a25e9dd52a4cada2aa7c530f408e80c
chazdean <chaz.dean@shopify.com>
2021-08-13 23:15:36 +0000

    added chai testing

--- test/mapTest.js
+++ test/mapTest.js
@@ -1,12 +1,24 @@
 const map = require('../map');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-const words = ["ground", "control", "to", "major", "tom"];
+const words = ['ground', 'control', 'to', 'major', 'tom'];
 
 const results1 = map(words, word => word[0]);
 const results2 = map(words, word => word.toUpperCase());
 const results3 = map(words, word => word.length);
 
-assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
-assertArraysEqual(results2, [ 'GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM' ]);
-assertArraysEqual(results3, [ 6, 7, 2, 5, 3 ]);
+describe("#map", () => {
+
+  it("returns [ 'g', 'c', 't', 'm', 't' ] for map(['ground', 'control', 'to', 'major', 'tom'], word => word[0]", () => {
+    assert.deepEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+  });
+
+  it("returns [ 'GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM' ] for map(['ground', 'control', 'to', 'major', 'tom'], word => word.toUpperCase())", () => {
+    assert.deepEqual(results2, [ 'GROUND', 'CONTROL', 'TO', 'MAJOR', 'TOM' ]);
+  });
+
+  it("returns [ 6, 7, 2, 5, 3 ] for map(['ground', 'control', 'to', 'major', 'tom'], word => word.length)", () => {
+    assert.deepEqual(results3, [ 6, 7, 2, 5, 3 ]);
+  });
+  
+});
