a1970fd6199ff7ac69902f1cd5388568b55debba
chazdean <chaz.dean@shopify.com>
2021-08-13 20:23:01 +0000

    added chai testing

--- eqObjects.js
+++ eqObjects.js
@@ -7,9 +7,10 @@ const eqObjects = function(object1, object2) {
   }
   for (const key of Object.keys(object1)) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (object1[key].length !== object2[key].length) {
+        return false;
+      }
       result = eqArrays(object1[key], object2[key]);
-    } else if (object1[key] !== object2[key]) {
-      return false;
     }
   }
   return result;
--- test/eqObjectsTest.js
+++ test/eqObjectsTest.js
@@ -1,20 +1,30 @@
-const assertEqual = require('../assertEqual');
 const eqObjects = require('../eqObjects');
+const assert = require('chai').assert;
 
-//TESTING PRIMITIVE DATA
-console.log("RUNNING TEST ON PRIMITIVE DATA");
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
-
 const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
-//TESTING NON-PRIMITIVE DATA
-console.log("RUNNING TEST ON NON-PRIMITIVE DATA");
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
+
+describe("#eqObjects", () => {
+  //TESTING PRIMITIVE DATA
+  it("returns true for ab = ba", () => {
+    assert.strictEqual(eqObjects(ab, ba), true);
+  });
+
+  it("returns false for ab = abc", () => {
+    assert.strictEqual(eqObjects(ab, abc), false);
+  });
+  //TESTING NON-PRIMITIVE DATA
+  it("returns true for cd = dc", () => {
+    assert.strictEqual(eqObjects(cd, dc), true);
+  });
+
+  it("returns false for cd = cd2", () => {
+    assert.strictEqual(eqObjects(cd, cd2), false);
+  });
+  
+});
+
