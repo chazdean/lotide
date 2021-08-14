1cf9422237a73e6d7283f26c60f372c1270b410f
chazdean <chaz.dean@shopify.com>
2021-08-14 00:01:24 +0000

    minor changes to look

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -4,19 +4,19 @@ const assert = require('chai').assert;
 describe("#eqArrays", () => {
 
   it('returns true for [1, 2, 3], [1, 2, 3]', () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
   });
 
   it("returns false for [1, 2, 3], [3, 2, 1]", () => {
-    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
   });
 
   it("returns true for ['1', '2', '3'], ['1', '2', '3']", () => {
-    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
   });
 
   it("returns false for ['1', '2', '3'], ['1', '2', 3]", () => {
-    assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
   });
 
 });
