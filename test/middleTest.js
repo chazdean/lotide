a115df6f099e665918cec637333e3db18df46994
chazdean <chaz.dean@shopify.com>
2021-08-14 00:00:28 +0000

    fixed syntax error

--- test/middleTest.js
+++ test/middleTest.js
@@ -4,27 +4,27 @@ const middle = require('../middle');
 describe("#middle", () => {
 
   it("returns [] for [1]", () => {
-    assert.strictEqual(middle([1]), []);
+    assert.deepEqual(middle([1]), []);
   });
 
   it("returns [1, 2] for []", () => {
-    assert.strictEqual(middle([1, 2]), []);
+    assert.deepEqual(middle([1, 2]), []);
   });
 
   it("returns [1, 2, 3] for [2]", () => {
-    assert.strictEqual(middle([1, 2, 3]), [2]);
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
 
   it("returns [1, 2, 3, 4, 5]) for [3]", () => {
-    assert.strictEqual(middle([1, 2, 3, 4, 5]), [3]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
 
   it("returns [1, 2, 3, 4] for [2, 3]", () => {
-    assert.strictEqual(middle([1, 2, 3, 4]), [2, 3]);
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
 
   it("returns [1, 2, 3, 4, 5, 6] for [3, 4]", () => {
-    assert.strictEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
   
 });
